fc8a6d6904b7f30d51eb0ac8206ac9bc627c2c36
Alexandra <alexandrita@ukr.net>
2019-09-04 21:26:05 +0300

    Add

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-
-function makeRequst2(url) {
+function makeRequest(url) {
   return fetch(url).then(res => 
     res.json()
   )
 }
 
-makeRequst2('https://dog.ceo/api/breeds/list/all')
+makeRequest('https://dog.ceo/api/breeds/list/all')
   .then(response => createList(response.message));
 
 function createList(dogs) {
@@ -14,26 +13,24 @@ function createList(dogs) {
   const p = document.createElement('p');
   const ul = document.createElement('ul');
   const li = document.createElement('li');
-  let arrDogs = [];
 
   for(dog in dogs) {
-    arrDogs.push(dog);
     const clone = p.cloneNode();
     clone.innerText = dog;
     fragment.appendChild(clone);
 
     if (dogs[dog].length > 0){
       const cloneUl = ul.cloneNode();
-      for(let i=0; i<dogs[dog].length; i++){
+      dogs[dog].forEach((item, i) => {
         const cloneLi = li.cloneNode();
         cloneLi.innerText = dog + "-" + dogs[dog][i];
         fragmentChild.appendChild(cloneLi);
         clickHandler2(cloneLi, dog);
-      }
+      });
       cloneUl.appendChild(fragmentChild);
       clone.appendChild(cloneUl);
-
     }
+    
     clickHandler(clone, dog);
   }
   const list = document.getElementById('list');
@@ -52,7 +49,6 @@ function createImagesSlider(src){
   slider();     
 }
 
-
 function createImagesSlider2(src, list){
   let arrImg = [];
   for (let j = 0; j < src.length; j++){
@@ -60,67 +56,68 @@ function createImagesSlider2(src, list){
           arrImg.push(src[j]);
     }
   }
-  let ul = document.getElementById("imagesSlider"); 
-      for(let i = 0; i < ul.children.length; i++){     
-        let li = ul.children[i];
-        let img = li.children[0];
-        img.src = arrImg[i];
-      }  
-
-  slider();     
+    createImagesSlider(arrImg)
 }
 
+
+function makeRequestSlider(name) {
+  makeRequest(`https://dog.ceo/api/breed/${name}/images/random/10`)
+      .then(response => createImagesSlider(response.message))
+}
+
+function makeRequestSlider2(name, list) {
+  makeRequest(`https://dog.ceo/api/breed/${name}/images/random/10`)
+      .then(response => createImagesSlider(response.message, list))
+}
 
  function clickHandler(list, name) {
   list.onclick = ($event) => {
    $event.stopPropagation();  
-    makeRequst2(`https://dog.ceo/api/breed/${name}/images/random/10`)
-      .then(response => createImagesSlider(response.message))
+   makeRequestSlider(name); 
   };
  }
 
  function clickHandler2(list, name) {
   list.onclick = ($event) => {  
     $event.stopPropagation();
-    makeRequst2(`https://dog.ceo/api/breed/${name}/images`)
-      .then(response => createImagesSlider2(response.message, list))
+    makeRequestSlider2(name, list);
   };
  }
 
- function generateImage(src, name) {
-  const imgExist = document.querySelector('#images img');
-  const imgName = document.querySelector('#nameDog');
-  imgName.innerText = name.replace(/\b\w/g, l => l.toUpperCase());
-
-  if (!!imgExist) {
-    imgExist.src = src;
-
-    return;
-  }
-  const img = document.createElement('img');
-  img.src = src;
-  const container = document.getElementById('images');
-  container.appendChild(img);
-}
-
-
+//Слайдер
 function slider(){
-  let slides = document.querySelectorAll('#imagesSlider .slide');
-  let currentSlide = 0;
-  let slideInterval = setInterval(nextSlide,2000);
+let i = 1;
+    for(let li of carousel.querySelectorAll('li')) {
+      li.style.position = 'relative';
+      li.insertAdjacentHTML('beforeend', `<span style="position:absolute;left:0;top:0">${i}</span>`);
+      i++;
+    }
+    let width = 300;
+    let count = 1;
 
-  function nextSlide(){
-  slides[currentSlide].className = 'slide';
-  currentSlide = (currentSlide+1)%slides.length;
-  slides[currentSlide].className = 'slide showing';
-  }
+    let list = carousel.querySelector('ul');
+    let listElems = carousel.querySelectorAll('li');
+
+    let position = 0;
+
+    carousel.querySelector('.prev').onclick = function() {
+      position += width * count;
+      position = Math.min(position, 0)
+      list.style.marginLeft = position + 'px';
+    };
+
+    carousel.querySelector('.next').onclick = function() {
+      position -= width * count;
+      position = Math.max(position, -width * (listElems.length - count));
+      list.style.marginLeft = position + 'px';
+    };
 }
 
-function listForSearch(){
-  let arrDogs = [];
-  makeRequst2('https://dog.ceo/api/breeds/list/all')
-  .then(response => {
-    for (let [key, value] of Object.entries(response.message)) {
+
+//Search
+function listOption(msg){
+    let arrDogs = [];
+    for (let [key, value] of Object.entries(msg)) {
       arrDogs.push(key);
     }
     const fragment = document.createDocumentFragment();
@@ -132,26 +129,24 @@ function listForSearch(){
     }
     const listDogs = document.getElementById('dogs');
     listDogs.appendChild(fragment);
-  });
 }
+
+function listForSearch(){
+  makeRequest('https://dog.ceo/api/breeds/list/all')
+  .then(response => listOption(response.message));
+}
+
+
 listForSearch();
 
-
-
-
-let btn = document.getElementById("btn");
-let inputDogs = document.getElementById("inputDogs");
-btn.onclick = function($event) {
+function clickBtnSearch(){
+  let btn = document.getElementById("btn");
+  let inputDogs = document.getElementById("inputDogs");
+  btn.onclick = function($event) {
   event.preventDefault();
   let name = inputDogs.value;
-  makeRequst2(`https://dog.ceo/api/breed/${name}/images/random/10`)
-    .then((response) => {
-      createImagesSlider(response.message);
-      slider();
-    });
+  makeRequestSlider(name);
+}
 }
 
-
-
-
-
+clickBtnSearch();
